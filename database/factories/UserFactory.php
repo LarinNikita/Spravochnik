2720c8625144d103aec8de4731a6e336236967f4
Mr. Cormorant <uusers1350@gmail.com>
2022-12-13 18:22:47 +0700

    с сидером

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\Status;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -18,10 +20,20 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'FIO' => fake()->name(),
+            'account' => fake()->unique()->safeEmail(),
+            'password' => random_int(10000, 99999),
+            'isChief' => random_int(0, 1),
+            'msOutlookMail' => $this->faker->email,
+            'officeNumber' => random_int(100, 999),
+            'ipPhone' => random_int(1000, 9999),
+            'phoneNumber' => $this->faker->phoneNumber,
+
+            'post_id' => Post::get()->random()->id,
+            'status_id' => Status::get()->random()->id,
+            'os_id' => Status::get()->random()->id,
+
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\Os;
+use App\Models\Status;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +18,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Os::factory(4)->create();
+        Post::factory(20)->create();
+        Status::factory(3)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(50)->create();
     }
 }
