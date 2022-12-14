6b7acfbbbcd948227508a71d57a57429436d6dc3
Mr. Cormorant <uusers1350@gmail.com>
2022-12-14 10:13:03 +0700

    конфиг завершён

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Department;
 use App\Models\Post;
 use App\Models\Status;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -26,12 +27,13 @@ class UserFactory extends Factory
             'isChief' => random_int(0, 1),
             'msOutlookMail' => $this->faker->email,
             'officeNumber' => random_int(100, 999),
-            'ipPhone' => random_int(1000, 9999),
+            'ipPhone' => fake()->numberBetween(999, 99999),
             'phoneNumber' => $this->faker->phoneNumber,
 
             'post_id' => Post::get()->random()->id,
             'status_id' => Status::get()->random()->id,
             'os_id' => Status::get()->random()->id,
+            'department_id' => Department::get()->random()->id,
 
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Department;
 use App\Models\Post;
 use App\Models\Os;
 use App\Models\Status;
@@ -21,7 +22,8 @@ class DatabaseSeeder extends Seeder
         Os::factory(4)->create();
         Post::factory(20)->create();
         Status::factory(3)->create();
+        Department::factory(50)->create();
 
-        User::factory(50)->create();
+        User::factory(100)->create();
     }
 }
